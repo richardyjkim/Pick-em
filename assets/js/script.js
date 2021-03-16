d8ccefa7f2adfd540e1715aa99c78db6a799f3f2
Richard <richardyjkim@gmail.com>
2021-03-15 17:43:04 -0700

    html css enhanced

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 let startBtn = document.getElementById("start");
+let matchupBtn = document.getElementById("match-ups")
 let playBtn = document.getElementById("how-to-play");
+let leaderBtn = document.getElementById("leaderboard");
 let introSectionEl = document.getElementById("intro-section");
+let leaderboardEl = document.getElementById("leaderboard-section");
 let playSectionEl = document.getElementById("play-section");
 let startSectionEl = document.getElementById("start-section");
+let matchupSectionEl = document.getElementById("matchup-section");
 
 
 startBtn.addEventListener("click", function(event) {
@@ -16,5 +20,23 @@ playBtn.addEventListener("click", function(event) {
   startSectionEl.classList.add("display-none");
   introSectionEl.classList.add("display-none");
   playSectionEl.classList.remove("display-none");
+  matchupSectionEl.classList.add("display-none");
+  leaderboardEl.classList.add("display-none");
+});
 
+matchupBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  matchupSectionEl.classList.remove("display-none");
+  playSectionEl.classList.add("display-none");
+  startSectionEl.classList.add("display-none");
+  leaderboardEl.classList.add("display-none");
+});
+
+leaderBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  leaderboardEl.classList.remove("display-none");
+  playSectionEl.classList.add("display-none");
+  startSectionEl.classList.add("display-none");
+  matchupSectionEl.classList.add("display-none");
+  introSectionEl.classList.add("display-none");
 })
