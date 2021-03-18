d9bc34d658b266c6aeb6efff73881042890fe82d
Christopher Campbell <campbefs@gmail.com>
2021-03-17 22:59:54 -0700

    started work on My Picks section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 let startBtn = document.getElementById("start");
+let mypicksBtn = document.getElementById("mypicks");
 let matchupBtn = document.getElementById("match-ups")
 let playBtn = document.getElementById("how-to-play");
 let introSectionEl = document.getElementById("intro-section");
 let playSectionEl = document.getElementById("play-section");
 let startSectionEl = document.getElementById("start-section");
 let matchupSectionEl = document.getElementById("matchup-section");
+let mypicksSectionEl = document.getElementById("mypicks-section");
+
 
 
 startBtn.addEventListener("click", function(event) {
@@ -19,6 +22,7 @@ playBtn.addEventListener("click", function(event) {
   introSectionEl.classList.add("display-none");
   playSectionEl.classList.remove("display-none");
   matchupSectionEl.classList.add("display-none");
+  mypicksSectionEl.classList.add("display-none");
 });
 
 matchupBtn.addEventListener("click", function(event){
@@ -27,4 +31,14 @@ matchupBtn.addEventListener("click", function(event){
   playSectionEl.classList.add("display-none");
   startSectionEl.classList.add("display-none");
   introSectionEl.classList.add("display-none");
+  mypicksSectionEl.classList.add("display-none");
+});
+
+mypicksBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  matchupSectionEl.classList.add("display-none");
+  playSectionEl.classList.add("display-none");
+  startSectionEl.classList.add("display-none");
+  introSectionEl.classList.add("display-none");
+  mypicksSectionEl.classList.remove("display-none");
 });
